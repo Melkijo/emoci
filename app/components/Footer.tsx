20591865cf142555172cdfbe2cf58304b501a8da
melkijo <jonathanandaraa@gmail.com>
2025-09-07 15:45:11 +0700

    fix: footer and metadata

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Footer() {
     return(
         <footer id="footer" className="py-8 bg-primary/5">
-            <div className="max-w-7xl mx-4 p-4 sm:px-6 lg:px-8 bg-primary rounded-lg ">
-            <div className="flex justify-between items-center ">
+            <div className="max-w-7xl mx-auto  ">
+            <div className="flex justify-between items-center mx-4 bg-primary p-4 sm:px-6 lg:px-8  rounded-lg">
             <p className="text-sm text-white">© {new Date().getFullYear()} Emoci. All rights reserved.</p>
                 <Link href="/privacy-policy" className="text-sm text-white hover:text-white/80 transition-colors duration-200">Privacy Policy</Link>
                
